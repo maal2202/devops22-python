9a9e1125fd61f0c51dc78bd70f25ef0e2df957eb
Martin Alfredson <martin.alfredson@hotmail.com>
2022-09-01 15:20:13 +0200

    Test1 Python

---
+++ lesson_2/maal2202_lesson_2/test1.py
@@ -0,0 +1,30 @@
+# test print
+
+# Variabler
+a = 8
+b = 3
+c = "Hello World"
+d = True
+e = None
+
+# Heltal
+print(a//b)
+
+# Decimaler
+print(a/b)
+
+# String
+print(c)
+
+# Boolean
+print(a>b)
+print(b>a)
+print(bool(a>b))
+
+# None
+print(e)
+
+# f-string
+print(f'Här följer alla de variabler som användes: {a}, {b}, {c}, {d}, {e}')
+
+print(f'a + b blir: {a+b}')
