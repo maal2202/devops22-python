cada54c5ae0bfbde9f6508652ea841dc97a07d7e
Martin Alfredson <martin.alfredson@hotmail.com>
2022-09-01 17:33:14 +0200

    add code to avoid syntax error when type do not match

--- lesson_2/maal2202_lesson_2/test2.py
+++ lesson_2/maal2202_lesson_2/test2.py
@@ -1,9 +1,6 @@
 # Test 2 inputs
 
 # Variabler
-from tkinter import N
-
-
 name = input("What is your name?\n")
 print(f'Hello {name}, good morning')
 
@@ -12,8 +9,11 @@ print(f'Name is of type {type(name)}')
 
 # Variabel för ålder, med input
 age = input(f'How old are you {name}?\n')
-print(f'Name is of type {type(age)}')
-print(f'Times two that is {int(age)*int(2)}')
+print(f'Age is of type {type(age)}')
+if age.isdigit():
+    print (f'Oh so young, I am double your age, {int(age)*2}')
+else:
+    print (f'{name} you are so dumb!')
 
 # Print a string times two
 greet = input(f'How do you greet people? {name}?\n')
@@ -21,4 +21,7 @@ print(f'Oh! {greet * 2}')
 
 # Print a float input and divid it by 3.5
 zip = input(f'What is your zipcode {name}?\n')
-print(f'that divided by 3.5 is {float(zip)/3.5}')
+if zip.isdigit():
+    print(f'that divided by 3.5 is {float(zip)/3.5}')
+else:
+    print(f'Syntax error, do not compute! Good bye {name}!')
