e49d1441d689416584ab0ca68338f1ee401f583d
Martin Alfredson <martin.alfredson@hotmail.com>
2022-09-05 15:56:44 +0200

    övning5, sorting

--- lesson_3/maal2202_lesson_3/test5.py
+++ lesson_3/maal2202_lesson_3/test5.py
@@ -1,1 +1,21 @@
 # Övning5 - Sorting
+
+cars = []
+cars.extend(["volvo", "saab", "audi", "bmw", "porche", "ferrari", "lotus", "ford", "tesla", "nio"])
+
+print(f'ordinarie inmatning {cars}')
+
+# Koden för sorted() och reversed() genererar endast sortering sagd kod
+print(f'sorted {sorted(cars)}')
+print(f'reversed {list(reversed(cars))}')
+
+print(f'ordinarie inmatning {cars}')
+
+# Sorteringen ifrån .sort och .reverse behålls fortsatt nedåt i koden
+cars.sort()
+print(f'efter cars.sort() {cars}')
+
+cars.reverse()
+print(f'efter cars.reverse() {cars}')
+
+print(f'Sorteringen är fortsatt baklänges {cars}')
