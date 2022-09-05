2f2a3d893d8ccd327181f081c39a0bc9c971871b
Martin Alfredson <martin.alfredson@hotmail.com>
2022-09-05 11:12:17 +0200

    Övning 2 korrigerad efter given lösning

--- lesson_3/maal2202_lesson_3/test2.py
+++ lesson_3/maal2202_lesson_3/test2.py
@@ -1,55 +1,56 @@
 # Uppgift B, lektion 3
 
+# List över alla personer, vilka läggs in i listan senare med .append
+persons = []
+
 # Person_1
-name1 = input("Name of person 1:\n>")
-age1 = input("How old is person 1:\n>")
-shoe1 = input("Shoe size of person 1:\n>")
+name1 = input("Name of person 1:\n>").lower()
+age1 = input("How old is person 1:\n>").lower()
+shoe1 = input("Shoe size of person 1:\n>").lower()
+person1 = (name1, age1, shoe1)
+persons.append(person1)
 
 # Person_2
-name2 = input("Name of person 2:\n>")
-age2 = input("How old is person 2:\n>")
-shoe2 = input("Shoe size of person 2:\n>")
+name2 = input("Name of person 2:\n>").lower()
+age2 = input("How old is person 2:\n>").lower()
+shoe2 = input("Shoe size of person 2:\n>").lower()
+person2 = (name2, age2, shoe2)
+persons.append(person2)
 
 # Person_3
-name3 = input("Name of person 3:\n>")
-age3 = input("How old is person 3:\n>")
-shoe3 = input("Shoe size of person 3:\n>")
+name3 = input("Name of person 3:\n>").lower()
+age3 = input("How old is person 3:\n>").lower()
+shoe3 = input("Shoe size of person 3:\n>").lower()
+person3 = (name3, age3, shoe3)
+persons.append(person3)
 
-#Tuple av de tre personerna
-tup = [(name1, age1, shoe1), (name2, age2, shoe2), (name3, age3, shoe3)]
-print(tup)
+sorted_shoe = sorted(persons, key=lambda y: y[2])
+sorted_age = sorted(persons, key=lambda y: y[1])
 
-tup.sort(key=lambda y: y[1])
-tup.reverse()
-print(f'The oldet person is {max(tup[0])} with a shoe size of {max(tup[0])}')
+oldest = sorted_age[2]
+median_shoe = sorted_shoe[1]
 
+print(f'The oldest person is {oldest[0].title()}, with a shoe size of {oldest[2]}')
+print(f'{median_shoe[0].title()} has the median shoe size and is {median_shoe[1]} years old.')
 
-#for item in tup:
-#print(f' The oldest person is {max(tup[0])}')
-
-# print ("Max value element : ", max(tup))
-
-#name = input("What is your name?\n")
-#print(f'Hello {name}, good morning')
-
-# Skriver ut vilken typ variabeln är, i detta fall string (class 'str')
-#print(f'Name is of type {type(name)}')
-
-# Variabel för ålder, med input
-#age = input(f'How old are you {name}?\n')
-#print(f'Age is of type {type(age)}')
-#if age.isdigit():
-#    print (f'Oh so young, I am double your age, {int(age)*2}')
-#else:
-#    print (f'{name} you are so dumb!')
-
-# Print a string times two
-#greet = input(f'How do you greet people? {name}?\n')
-#print(f'Oh! {greet * 2}')
-
-# Print a float input and divid it by 3.5
-#zip = input(f'What is your zipcode {name}?\n')
-#if zip.isdigit():
-#    print(f'that divided by 3.5 is {float(zip)/3.5}')
-#else:
-#    print(f'Syntax error, do not compute! Good bye {name}!')
+# Sökfunktion, krävs att man fyller i någon utav de tre sökvariablerna "name" "age" "shoe" följt av värdet du letar efter, 
+# exempelvis "shoe 43", "age 34", eller "name martin". 
+searches = {
+    "name": {
+        str(name1): person1,
+        str(name2): person2,
+        str(name3): person3
+    },
+    "age": {
+        str(age1): person1,
+        str(age2): person2,
+        str(age3): person3
+    },
+    "shoe": {
+        str(shoe1): person1,
+        str(shoe2): person2,
+        str(shoe3): person3
+    }
+}
+prop, value = input("Enter prop value: ").split(" ")
+print(searches[prop][value])
