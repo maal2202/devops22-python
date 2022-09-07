baab039b1ed768ec7b11697374409f0ee3a1a122
Martin Alfredson <martin.alfredson@hotmail.com>
2022-09-07 11:59:22 +0200

    uppdaterad test1.py

--- lesson_4/maal2202_lesson_4/test1.py
+++ lesson_4/maal2202_lesson_4/test1.py
@@ -2,13 +2,8 @@
 
 x = int(input("Skriv en siffra: "))
 
-if x.isdigit:
-    
 if (x % 2) == 0: 
-print("Even number")
+    print("Even number")
 
 else:
-print("Odd number")
-
-else
-print("fel fel fel")
+    print("Odd number")
