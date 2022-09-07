77fb977b6011b3a9216edb39b7805fa2f2dd8caa
Martin Alfredson <martin.alfredson@hotmail.com>
2022-09-07 11:47:11 +0200

    uppdaterad

--- lesson_4/maal2202_lesson_4/test1.py
+++ lesson_4/maal2202_lesson_4/test1.py
@@ -1,7 +1,14 @@
 # Lektion 4 - Övning 1
 
 x = int(input("Skriv en siffra: "))
+
+if x.isdigit:
+    
 if (x % 2) == 0: 
-    print("Even number")
+print("Even number")
+
 else:
-    print("Odd number")
+print("Odd number")
+
+else
+print("fel fel fel")
