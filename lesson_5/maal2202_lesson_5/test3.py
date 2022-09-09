e29aa48b4a58a6af460ee37b98684da3922a23f6
Martin Alfredson <martin.alfredson@hotmail.com>
2022-09-09 11:56:57 +0200

    Övningsuppgift 3

---
+++ lesson_5/maal2202_lesson_5/test3.py
@@ -0,0 +1,8 @@
+# lektion 5 - Övning 3
+
+price = int(input("How much did your new car cost in USD?\n"))
+
+if price > 20620:
+    print(f'Your car cost more than 1\u20BF \U0001F602')
+else:
+    print(f'Your car cost less than 1\u20BF \U0001F605')
