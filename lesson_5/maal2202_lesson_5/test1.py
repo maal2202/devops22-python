828e945e7a01f0d71b1be8044d700c75fb112339
Martin Alfredson <martin.alfredson@hotmail.com>
2022-09-09 11:12:30 +0200

    Övningsuppgift 1

---
+++ lesson_5/maal2202_lesson_5/test1.py
@@ -0,0 +1,28 @@
+# Lektion 5 - Övning 1
+
+firstname = "john"
+lastname = "smith"
+tele = "00468123456789"
+
+# 1
+print (f'Firstname: {firstname}, Lastname: {lastname}, Phonenumber: {tele}')
+
+# 2
+fullname = firstname + lastname
+print(fullname)
+
+# 3
+fullname = (f'{firstname} {lastname}')
+print(fullname)
+
+# 4
+print(f' len() of fullname + firstname + lastname: {len(fullname) + len(firstname) + len(lastname)}')
+
+# 5
+print (f'Firstname: {firstname}, Lastname: {lastname}\nPhonenumber: {tele}')
+
+# 6
+print(str("Fullname: ") + fullname + str(", Phonenumber: ") + tele) # 6:1
+print(f'Fullname: {fullname}, Phonenumber: {tele}') # 6:2
+print("Fullname: {name}, Phonenumber: {phonenumber}".format(name=fullname, phonenumber=tele)) # 6:3
+print("Fullname: %s, Phonenumber: %s" % (fullname, tele)) # 6:4
