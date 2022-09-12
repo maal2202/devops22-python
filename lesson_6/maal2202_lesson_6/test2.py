ad75d1618bb69dbd41bbc69b184c5dee5206188a
Martin Alfredson <martin.alfredson@hotmail.com>
2022-09-12 16:06:14 +0200

    test2.py övning 2

---
+++ lesson_6/maal2202_lesson_6/test2.py
@@ -0,0 +1,38 @@
+# Lektion 6 - Övning 2
+import random
+import string
+from operator import mul
+from random import shuffle, choices, sample
+from typing import Counter
+from operator import itemgetter
+
+    # 1
+names = ["alpha", "bravo", "charlie", "delta", "echo", "foxtrot", "golf", "hotel"]
+names_random = []
+for i in range(100):
+    names_random.append(random.choice(names))
+print(names_random)
+
+    # 2
+c = Counter(names_random)
+print(c)
+
+    # 3
+print(c.most_common(3))
+
+    # 4
+print(f'{c.most_common()[:-1-1:-1]}')
+
+    # 5
+names_unique = list(dict.fromkeys(names_random))
+
+    # 5.1
+alphabetical = sorted(names_unique)
+print(alphabetical)
+
+    # 5.2
+print(random.sample(names_unique, len(names_unique)))
+
+    # 5.3
+alphabetical.reverse()
+print(alphabetical)
