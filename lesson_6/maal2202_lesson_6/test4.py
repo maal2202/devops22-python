e80249fa0019d37face54e8fd2c8c66cb6ba2b87
Martin Alfredson <martin.alfredson@hotmail.com>
2022-09-12 18:06:33 +0200

    test4.py övning 4

---
+++ lesson_6/maal2202_lesson_6/test4.py
@@ -0,0 +1,48 @@
+# Lektion 6 - Övning 4
+import random
+import string
+from operator import mul
+from random import shuffle, choices, sample
+from typing import Counter
+from operator import itemgetter
+from collections import deque
+
+    # 1
+names = ["adam", "bertil", "cesar", "david", "erik", "filip", "gustav", "helge", "ivar", "johan", "kalle", "ludvig"]
+names50 = []
+for i in range(50):
+    names50.append(random.choice(names))
+print(names50)
+
+    # 2
+queue = deque([], 10)
+
+for i in range(10):
+    queue.append(random.choice(names))
+print(queue)
+
+    # 3
+rand = random.randint(1, 10)
+while rand > 0:
+    queue.pop()
+    rand-=1
+print(queue)
+
+    # 4
+refill = (10-len(queue))
+finish = 40-refill
+while refill > 0:
+    queue.append(random.choice(names))
+    refill-=1
+print(queue)
+
+    # 5
+while finish > 10:
+    queue.pop()
+    finish-=1
+    queue.append(random.choice(names))
+print(queue)
+while finish > 0:
+    queue.pop()
+    finish-=1
+print(queue)
