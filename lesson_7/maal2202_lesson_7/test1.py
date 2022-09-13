b11fe294ea34637f3d6ce4d1d915e647817abd88
Martin Alfredson <martin.alfredson@hotmail.com>
2022-09-13 14:32:17 +0200

    Övning 1

---
+++ lesson_7/maal2202_lesson_7/test1.py
@@ -0,0 +1,36 @@
+# Lektion 7 - Övning 1
+from audioop import reverse
+import string
+    # 1
+def do_nothing():
+    pass
+do_nothing()
+    # 2.1
+def hello_world():
+    print("Hello World")
+hello_world()
+    # 2.2
+def result():
+    print(1==1.0)
+result()
+    # 2.3
+def abc():
+    abc = list(string.ascii_lowercase)
+    abc.sort(reverse=True)
+    print(abc)
+abc()
+    # 3.1
+def greet(name):
+    print(f'Hello {name}')
+greet("Pelle")
+    # 3.2
+def yell(word):
+    print(word.upper())
+yell("hej")
+    # 3.4
+def numbers(stop):
+    start = 1
+    while stop != start:
+        print(start)
+        start += 1
+numbers(7)
